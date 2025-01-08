c7bb3150a6fc1e7e16df91bdfacee836622ef040
DomenGoli <domen.goli@gmail.com>
2025-01-08 22:07:36 +0100

    fix config

--- src/js/config.js
+++ src/js/config.js
@@ -4,7 +4,7 @@
 // export const KEY = '<YOUR_KEY>';
 // export const MODAL_CLOSE_SEC = 2.5;
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes'
-export const TIMOUT_SEC = 10
-export const RESULTS_PER_PAGE = 10
-export const API_KEY = 'ba7d747e-dc44-4884-939f-45786c2766de'
+export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10
+export const KEY = 'ba7d747e-dc44-4884-939f-45786c2766de'
 export const MODAL_CLOSE_SEC = 2.5
