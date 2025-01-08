6c4a00064880410832a8e73192314f21807ce2e0
DomenGoli <domen.goli@gmail.com>
2025-01-08 22:15:40 +0100

    fix api link

--- src/js/config.js
+++ src/js/config.js
@@ -3,7 +3,7 @@
 // export const RES_PER_PAGE = 10;
 // export const KEY = '<YOUR_KEY>';
 // export const MODAL_CLOSE_SEC = 2.5;
-export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes'
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/'
 export const TIMEOUT_SEC = 10
 export const RES_PER_PAGE = 10
 export const KEY = 'ba7d747e-dc44-4884-939f-45786c2766de'
